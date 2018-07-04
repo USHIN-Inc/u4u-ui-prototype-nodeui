8b8d796e80b76b8140ecf9f9a382a4fa88574f18
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-07-04 04:04:38 -0700

    Tweaking CUIButtonComponent events.

--- app/scripts/cui.js
+++ app/scripts/cui.js
@@ -1,3 +1,9 @@
+function myButton1Click() {
+  alert('myButton1Click called');
+}
+function myButton2Click() {
+  alert('myButton2Click called');
+}
 var CUIContainer =
   new CUIOutlineContainer()
     .setComponent(4,
@@ -7,14 +13,23 @@ var CUIContainer =
         .setComponent(2, new CUINorthEastContainer())
         .setComponent(3, new CUIWestContainer())
         .setComponent(4, new CUIBalancedContainer()
-          .setComponent(4, new CUITextComponent('This is a Text Component'))
+          .setComponent(4, new CUITextComponent('Text'))
         )
         .setComponent(5, new CUIEastContainer())
         .setComponent(6, new CUISouthWestContainer())
         .setComponent(7, new CUISouthContainer())
         .setComponent(8, new CUISouthEastContainer())
     )
-    .setComponent(7, new CUIButtonComponent('This is a Button Component'));
+    .setComponent(1, new CUIButtonComponent(
+      'Button 1',
+      'myButton1Click()'
+    ))
+    .setComponent(3, new CUIButtonComponent('Button'))
+    .setComponent(7, new CUIButtonComponent(
+      'Button 2',
+      'myButton2Click()'
+    ))
+    ;
 
 document
   .getElementById('CUIContainer')
--- app/scripts/cuilib/components/CUIButtonComponent.js
+++ app/scripts/cuilib/components/CUIButtonComponent.js
@@ -1,12 +1,13 @@
 class CUIButtonComponent extends CUIComponent {
-  constructor(text = 'New Button Component') {
+  constructor(text = 'New Button Component', onclick = 'console.log(\'click\');') {
     super();
     this._text = text;
+    this._onclick = onclick;
   }
   render() {
     return ''
       + '<div class="cui-component cui-component-button cui-component-border">'
-      + '<button>' + this._text + '</button>'
+      + '<button onclick="' + this._onclick + '">' + this._text + '</button>'
       + '</div>';
   }
 }
