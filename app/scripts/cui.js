5aea48a759f4e9c395b406b76732120ff45008a2
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-07-12 00:30:47 -0700

    More refactoring CUI example

--- app/scripts/cui.js
+++ app/scripts/cui.js
@@ -3,32 +3,32 @@ var CUIContainer =
     .setComponent(4,
       new CUIBalancedContainer()
         .setComponent(0, new CUINorthWestContainer())
-        .setComponent(1, new CUINorthContainer())
+        .setComponent(1, new CUINorthContainer()
+          .setComponent(7, new CUIButtonComponent(
+              'Click me!',
+              'myButtonClick()'
+          )))
         .setComponent(2, new CUINorthEastContainer())
         .setComponent(3, new CUIWestContainer())
         .setComponent(4, new CUIBalancedContainer()
-          .setComponent(4, new CUITextComponent('Text')))
+          .setComponent(4, new CUITextComponent('Click that button up there!')))
         .setComponent(5, new CUIEastContainer())
         .setComponent(6, new CUISouthWestContainer())
         .setComponent(7, new CUISouthContainer())
         .setComponent(8, new CUISouthEastContainer())
-    )
-    .setComponent(1, new CUIButtonComponent(
-      'Button 1',
-      'myButton1Click(CUIContainer)'
-    ));
+    );
 
-var num = 0;
-function myButton1Click(container) {
-  (container
-    .getComponent(4)
-      .getComponent(4)
-        .getComponent(4)).text = '<h1>' + (++num) + '</h1>';
-  renderContainer();
-}
-    
 function renderContainer() {
   document
     .getElementById('CUIContainer')
     .innerHTML = CUIContainer.render();
+}
+    
+var count = 0;
+function myButtonClick() {
+  (CUIContainer
+    .getComponent(4)
+      .getComponent(4)
+        .getComponent(4)).text = '<h1>' + (++count) + '</h1>';
+  renderContainer();
 }
