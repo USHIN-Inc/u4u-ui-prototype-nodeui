92a7d49b840bfe1503316eebf7fea6383944f579
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-07-30 18:31:31 -0700

    Started working on nodeui interface Removed jQuery implementation

--- app/scripts/node.js
+++ app/scripts/node.js
@@ -30,27 +30,3 @@ class Node {
     }
   }
 }
-
-var root = new Node('Fruit', 'topic');
-root.add(new Node('Fruit is good for you', 'thought'));
-root.add(new Node('Eat', 'action'))
-  .add(new Node('Give', 'action'))
-  .add(new Node('Plant', 'action'))
-  .add(new Node('Preserve', 'action'))
-  .add(new Node('Squash', 'action'))
-  .add(new Node('Ferment', 'action'))
-  .add(new Node('Cook', 'action'))
-  .add(new Node('Refine', 'action'))
-  .add(new Node('Food', 'topic'));
-
-var leaf = null;
-
-leaf = root.children.find(node => node.text === 'Fruit is good for you');
-leaf.add(new Node('Fruit is bad for you', 'thought'));
-
-leaf = root.children.find(node => node.text === 'Preserve');
-leaf.add(
-  new Node('Refrigerator', 'need').add(
-    new Node('Temperature Control', 'need')
-  )
-);
