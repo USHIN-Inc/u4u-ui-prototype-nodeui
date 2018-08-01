eb9815883e343e75a4b13f6ddf4ad2e525bbef6c
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-07-31 20:42:45 -0700

    Added nodeui components.

--- app/scripts/nodeui.js
+++ app/scripts/nodeui.js
@@ -1,30 +1,33 @@
-var nodeui = (() => {
+var root = new Node('Fruit', 'topic');
 
-    var root = new Node('Fruit', 'topic');
-    root.add(new Node('Fruit is good for you', 'thought'))
-        .add(new Node('Eat', 'action'))
-        .add(new Node('Give', 'action'))
-        .add(new Node('Plant', 'action'))
-        .add(new Node('Preserve', 'action'))
-        .add(new Node('Squash', 'action'))
-        .add(new Node('Ferment', 'action'))
-        .add(new Node('Cook', 'action'))
-        .add(new Node('Refine', 'action'))
-        .add(new Node('Food', 'topic'));
+root.add(new Node('Fruit is good for you', 'thought'))
+    .add(new Node('Eat', 'action'))
+    .add(new Node('Give', 'action'))
+    .add(new Node('Plant', 'action'))
+    .add(new Node('Preserve', 'action'))
+    .add(new Node('Squash', 'action'))
+    .add(new Node('Ferment', 'action'))
+    .add(new Node('Cook', 'action'))
+    .add(new Node('Refine', 'action'))
+    .add(new Node('Food', 'topic'));
 
-    var leaf = null;
-    leaf = root.children.find(node => node.text === 'Fruit is good for you');
-    leaf.add(new Node('Fruit is bad for you', 'thought'));
-    
-    leaf = root.children.find(node => node.text === 'Preserve');
-    leaf.add(
-        new Node('Refrigerator', 'need').add(
-            new Node('Temperature Control', 'need')
-        )
-    );
+var leaf = null;
+leaf = root.children.find(node => node.text === 'Fruit is good for you');
+leaf.add(new Node('Fruit is bad for you', 'thought'));
 
-    return { root: root };
+leaf = root.children.find(node => node.text === 'Preserve');
+leaf.add(
+    new Node('Refrigerator', 'need').add(
+        new Node('Temperature Control', 'need')
+    )
+);
 
-})();
-
-$('#nodes').html('<h1>What\'s up!</h1>');
+function generateUnorderedListFromNode(node) {
+    var output = '';
+    output += '<ul><li><a href="#" data-id="' + node.id + '" data-tag="' + node.tag +'">' + node.text + '</a>';
+    node.children.forEach(child => output += generateUnorderedListFromNode(child));
+    output += '</li></ul>';
+    return output;
+}
+$('#nodeui').html(generateUnorderedListFromNode(root));
+$('#nodeui a').on('click', e => console.log(e));
