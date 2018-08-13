25984d72e26b7c3830c23fb748e1d07cea88679c
Alex Garcia <hi.im.alex.j.garcia@gmail.com>
2018-08-13 05:09:25 -0700

    Refactoring nodeui code.

--- app/scripts/nodeui.js
+++ app/scripts/nodeui.js
@@ -1,166 +1,80 @@
-var root = getTree();
-render(root);
+(function(){
 
-function getTree() {
-    var root = new Node('Fruit', 'topic')
-        .add(new Node('Fruit is good for you', 'thought')
-            .add(new Node('Fruit is bad for you', 'thought')))
-        .add(new Node('Eat', 'action'))
-        .add(new Node('Give', 'action'))
-        .add(new Node('Plant', 'action'))
-        .add(new Node('Preserve', 'action')
-            .add(new Node('Refrigerator', 'need')
-                .add(new Node('Temperature Control', 'need'))))
-        .add(new Node('Squash', 'action'))
-        .add(new Node('Ferment', 'action'))
-        .add(new Node('Cook', 'action'))
-        .add(new Node('Refine', 'action'))
-        .add(new Node('Food', 'topic')
-            .add(new Node('Alex Garcia', 'person')
-                .add(new Node('Big fan of food', 'feeling'))));
+    var nodes = window.__INITIAL_STATE__.data || [];
 
-    return root;
-};
-
-function render(root) {
-    renderNavPane(root);
-    renderUIPane(root);
-}
-
-function renderNavPane(root) {
-    function generateUnorderedListFromNode(node) {
-        var output = '';
-        output += '<ul><li><a href="/" data-id="' + node.id + '" data-tag="' + node.tag +'"><img style="height: 12px;" alt="" src="/img/' + node.tag + 's.png" /> ' + node.text + '</a>';
-        node.children.forEach(child => output += generateUnorderedListFromNode(child));
-        output += '</li></ul>';
-        return output;
-    }
-    $('#nodeui').html(generateUnorderedListFromNode(root));
-    $('#nodeui a').on('click', e => { e.preventDefault(); redrawUI(e.target.dataset.id); });
-}
-
-function renderUIPane(root) {
-    redrawUI(root.id);
-};
-
-function redrawUI(focusNodeId) {
-    var focusNode;
-    var parentNode;
-    var childNodes;
-
-    function findNodeById(id, tree) {
-        if (tree.id === id) {
-            focusNode = tree;
-        } else {
-            tree.children.forEach(node => findNodeById(id, node));
-        }
+    var inflectedTag = {
+        "merit": "merits",
+        "person": "people",
+        "action": "actions",
+        "topic": "topics",
+        "need": "needs",
+        "feeling": "feelings",
+        "thought": "thoughts",
+        "fact": "facts"
     }
 
-    function findParentNodeByChildId(id, tree) {
-        if (tree.children.find(node => node.id === id)) {
-            parentNode = tree;
-        } else {
-            tree.children.forEach(node => findParentNodeByChildId(id, node));
-        }
+    // Check if route is valid, otherwise set route to root node GUID
+    if (location.hash === "") {
+        var rootNode = nodes.find(node => node.parent_id === null);
+        window.history.replaceState(null, null, "#!/" + rootNode.id);
     }
 
-    findNodeById(focusNodeId, root);
-    findParentNodeByChildId(focusNodeId, root);
-    childNodes = focusNode.children;
-
-    $('#merits ul').empty();
-    $('#people ul').empty();
-    $('#actions ul').empty();
-    $('#topics ul').empty();
-    $('#needs ul').empty();
-    $('#feelings ul').empty();
-    $('#thoughts ul').empty();
-    $('#facts ul').empty();
-
-    $('#focus').html('<span class="focus">' + focusNode.text + '</span>');
-
-    if (parentNode) {
-        switch (parentNode.tag) {
-            case 'merit':
-                $('#merits ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'person':
-                $('#people ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'action':
-                $('#actions ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'topic':
-                $('#topics ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'need':
-                $('#needs ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'feeling':
-                $('#feelings ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'thought':
-                $('#thoughts ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-            case 'fact':
-                $('#facts ul').append('<li><a href="#" data-id="' + parentNode.id + '" data-tag="' + parentNode.tag +'">' + parentNode.text + '</a></li>');
-                break;
-        }
-    }
-
-    childNodes.forEach(childNode => {
-        switch (childNode.tag) {
-            case 'merit':
-                $('#merits ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'person':
-                $('#people ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'action':
-                $('#actions ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'topic':
-                $('#topics ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'need':
-                $('#needs ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'feeling':
-                $('#feelings ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'thought':
-                $('#thoughts ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-            case 'fact':
-                $('#facts ul').append('<li><a href="#" data-id="' + childNode.id + '" data-tag="' + childNode.tag +'">' + childNode.text + '</a></li>');
-                break;
-        }
-    })
-
-    $('#semui a').on('click', e => { e.preventDefault(); redrawUI(e.target.dataset.id); });
-
-    $.contextMenu({
-        selector: '#semui li', 
-        callback: function(key, options) {
-            var deletedNodeId = $(this).find('a').first().data('id');
+    // Render the node collection
+    renderSemUINav(nodes, "#semui-view-navigation");
+    renderSemUIRegions(nodes, "#semui-view-regions");
 
 
-            console.log( deletedNodeId );
-        },
-        items: {
-            "delete": {name: "Delete", icon: "delete"},
-        }
+    // Listen for route changes
+    $(window).on('hashchange', function(e) {
+        renderSemUINav(nodes, "#semui-view-navigation");
+        renderSemUIRegions(nodes, "#semui-view-regions");
     });
 
-    $.contextMenu({
-        selector: '#semui div', 
-        callback: function(key, options) {
-            var m = "clicked: " + key;
-            window.console && console.log(m) || alert(m); 
-        },
-        items: {
-            "add": {name: "Add", icon: "add"}
-        }
-    });
+    // Render functions
+    function renderSemUINav(nodes, el) {
+        var selectedNodeID = location.hash.replace("#!/", "");
+        var selectedNode = nodes.find(node => node.id.toString() === selectedNodeID);
 
-}
+        function traverseNodes(nodes, rootNode) {
+            var output = '';
+            output += '<ul><li>';
+            output += '<a ';
+            if (rootNode.id === selectedNode.id) {
+                output += 'class="selected" ';
+            }
+            output += 'href="#!/' + rootNode.id + '" data-id="' + rootNode.id + '" data-tag="' + rootNode.tag +'">'
+            output += '<img style="height: 12px;" alt="" src="/img/' + inflectedTag[rootNode.tag] + '.png" /> ' + rootNode.title + '</a>';
+            nodes.filter(node => node.parent_id === rootNode.id).forEach(currentNode => {
+                output += traverseNodes(nodes, currentNode);
+            });
+            output += '</li></ul>';
+            return output;
+        }
+        $(el).html(traverseNodes(nodes, nodes.find(node => node.parent_id === null)));
+    };
+
+    function renderSemUIRegions(nodes, el) {
+        var selectedNodeID = location.hash.replace("#!/", "");
+        var selectedNode = nodes.find(node => node.id.toString() === selectedNodeID);
+
+        var nodesToRender = [];
+        nodesToRender.push(nodes.filter(node => node.id === selectedNode.parent_id));
+        nodesToRender.push(nodes.filter(node => node.parent_id === selectedNode.id));
+        nodesToRender = nodesToRender.reduce((acc, val) => acc.concat(val), []);
+
+        $(el).empty();
+
+        $(el).append('<div id="semui-view-region-focus"><span class="focus">' + selectedNode.title + '</span></div>');
+
+        var output = "";
+        ["merit", "person", "action", "topic", "need", "feeling", "thought", "fact"].forEach(tag => {
+            output += '<div id="semui-view-region-' + inflectedTag[tag] + '"><ul>';
+            nodesToRender.filter(node => node.tag === tag).forEach(node => {
+                output += '<li><a href="#!/' + node.id + '" data-id="' + node.id + '" data-tag="' + node.tag +'">' + node.title + '</a></li>';
+            });
+            output += '</ul></div>';
+        });
+        $(el).append(output);
+    };
+
+})();
